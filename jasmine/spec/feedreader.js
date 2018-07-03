c273926610d63745151646794837ee3a64020ed7
bluessista <inga@bluessista.com>
2018-07-03 22:18:28 +0200

    tests: suite for Initial Feeds and New Feed Selection

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -80,5 +80,45 @@ $(function() {
          });
     });
 
+    /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', () => {
 
+        beforeEach(done => {
+            loadFeed(0, done);
+        });
+
+        /* TODO: Write a test that ensures when the loadFeed
+         * function is called and completes its work, there is at least
+         * a single .entry element within the .feed container.
+         * Remember, loadFeed() is asynchronous so this test will require
+         * the use of Jasmine's beforeEach and asynchronous done() function.
+         */
+        it('has at least a single entry', () => {
+            expect($('.feed .entry').length).toBeGreaterThan(0);
+        });
+    });
+
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', () => {
+        var contentInitial, contentNew;
+
+        beforeEach(done => {
+            loadFeed(1, () => {
+                //Content of .feed before selecting a new topic
+                contentInitial = $('.feed').html();
+                done();
+            });
+        });
+        /* TODO: Write a test that ensures when a new feed is loaded
+         * by the loadFeed function that the content actually changes.
+         * Remember, loadFeed() is asynchronous.
+         */
+        it('changes content if new feed topic is chosen', (done) => {
+            loadFeed(2, () => {
+                contentNew = $('.feed').html();
+                expect(contentInitial).not.toBe(contentNew);
+                done();
+            });
+        });
+    });
 }());
